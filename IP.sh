8ac7500bbcf896e7398ecbc38b48664dc3824242
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2020-04-25 02:13:58 +0200

    Create IP.sh

---
+++ IP.sh
@@ -0,0 +1,34 @@
+#/data/data/com.termux/files
+#AL-Alamy-styling V 1.0.0
+#Channel
+#https://www.youtube.com/channel/UCQuGjfmo04jDd6zlBscslGQ
+# A7Y Team
+
+black='\e[99;30m' # Black
+
+red='\e[99;31m' # Red
+
+green='\e[99;32m' # Green
+
+yellow='\e[99;33m' # Yellow
+
+blue='\e[99;34m' # Blue
+
+purple='\e[99;35m' # Purple
+
+cyan='\e[99;36m' # Cyan
+
+white='\e[99;37m' # White
+clear
+
+printf "/$$            /$$$$$$                /$$ "         
+printf "|__/           /$$__  $$              |__/"         
+printf " /$$ /$$$$$$$ | $$  \__//$$$$$$        /$$  /$$$$$$ "
+printf "| $$| $$__  $$| $$$$   /$$__  $$      | $$ /$$__  $$"
+printf "| $$| $$  \ $$| $$_/  | $$  \ $$      | $$| $$  \ $$"
+printf "| $$| $$  | $$| $$    | $$  | $$      | $$| $$  | $$"
+printf "| $$| $$  | $$| $$    |  $$$$$$/      | $$| $$$$$$$/"
+printf "|__/|__/  |__/|__/     \______/       |__/| $$____/" 
+printf "                                          | $$"      
+printf "                                          | $$"      
+printf "                                          |__/"      
